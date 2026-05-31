d8851bdd93a6eb58475d1fa9b2258b334748594a
FedeNicoTorres <federico.nico.torres@gmail.com>
2026-05-31 18:24:57 -0300

    Avance

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- ¡ASEGURATE DE QUE ESTA LÍNEA ESTÉ ACÁ!
+import './index.css' // <--- Este se queda
+// import './App.css' <--- Si ves esta línea, borrala por completo
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
