800ce52a185446589f85dd2fa31a75d1ac66dfdf
FedeNicoTorres <federico.nico.torres@gmail.com>
2026-05-31 18:28:11 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: 'https://github.com/FedeNicoTorres/estacionamiento/parking-test/', 
+  base: 'https://github.com/FedeNicoTorres/parking-test/', // <--- ¡REVISÁ ESTO! Tiene que coincidir con el nombre de tu repo
 })
